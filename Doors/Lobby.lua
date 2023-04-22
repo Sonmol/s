db4f8f8f9b1232e2809a2c1ab064cf14f3faf2a0
Sonmol <sonmolyt@gmail.com>
2023-04-22 12:16:54 -0500

    test

---
+++ Doors/Lobby.lua
@@ -0,0 +1,10 @@
+local lib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+local Window = lib:MakeWindow({Name = "Traicy Hub | Doors Lobby", IntroText = "Traicy Hub",HidePremium = true, SaveConfig = true, ConfigFolder = "Doors"})
+
+local plr = game:GetService("Players").LocalPlayer
+
+local _Visuals = Window:MakeTab({
+	Name = "Visuals",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
